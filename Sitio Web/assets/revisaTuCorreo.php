36666787a23085fe76877232eadee73ccd17a69d
Daniel Salinas <74389796+danielsalina@users.noreply.github.com>
2022-05-22 20:59:26 -0300

    Add files via upload

---
+++ Sitio Web/assets/revisaTuCorreo.php
@@ -0,0 +1,5 @@
+<p>Revisa tu correo</p>
+<b>Te hemos enviado un correo</b>
+<a href="http://localhost:8080/Sitio%20Web/">Regresar</a>
+<!-- --------------------⇑⇑⇑⇑⇑ --------------------------------------------->
+<!-- OJO AQUÍ, DEBEN COLOCAR EL PUERTO EN EL QUE ESTAN LEVANTANDO EL SITIO -->
